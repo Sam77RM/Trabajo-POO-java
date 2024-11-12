b59164722da68270f0938bada55ca9959e2195da
GoodF <GoodF@DESKTOP-QMBA8MR>
2024-11-12 11:41:21 -0500

    Notas generales

--- src/proyecto/poo/java/ProyectoPOOJava.java
+++ src/proyecto/poo/java/ProyectoPOOJava.java
@@ -5,6 +5,8 @@ public class ProyectoPOOJava {
 
     public static void main(String[] args) {
         System.out.println("Hello World");// TODO code application logic here
+        System.out.println("Recordar eliminar esta clase"
+                + "se trabajara con graficos");
     }
     
 }
