7cc9639469c7f9102601938565ddbff0cb08f5f7
GoodF <GoodF@DESKTOP-QMBA8MR>
2024-11-12 12:15:36 -0500

    Creacion de nueva clase de Prueba "New Class" - se eliminara

--- src/proyecto/poo/java/ProyectoPOOJava.java
+++ src/proyecto/poo/java/ProyectoPOOJava.java
@@ -6,7 +6,14 @@ public class ProyectoPOOJava {
     public static void main(String[] args) {
         System.out.println("Hello World");// TODO code application logic here
         System.out.println("Recordar eliminar esta clase"
-                + "se trabajara con graficos");
+                + "se trabajara con graficos"
+                + ""
+                + ""
+                + ""
+                + "ok");
+        
+        
+        
     }
     
 }
